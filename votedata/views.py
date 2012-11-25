0992c62b7da5f4d1923e0d9a53cd3642aeab0109
Gemma Hentsch <gemma@halfapenguin.com>
2012-11-25 14:22:13 +0000

    new view

--- votedata/views.py
+++ votedata/views.py
@@ -5,6 +5,10 @@ from votedata.models import *
 from django.template import RequestContext
 from django.views.decorators.cache import cache_page
 
+def different_elections(request):
+	elections = Election.objects.all()
+	return render_to_response("elections.html",{"elections",elections},RequestContext(request))
+
 @cache_page(60 * 15)
 def basic_view(request):
 	election = Election.objects.get(title="General Election 2010")
