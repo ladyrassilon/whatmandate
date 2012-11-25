e0faca4b8ca22312bfae74590e32d7e39c2a17f7
Gemma Hentsch <gemma@halfapenguin.com>
2012-11-25 14:38:18 +0000

    fix to the view

--- votedata/views.py
+++ votedata/views.py
@@ -7,9 +7,9 @@ from django.views.decorators.cache import cache_page
 
 def different_elections(request):
 	elections = Election.objects.all()
-	return render_to_response("elections.html",{"elections",elections},RequestContext(request))
+	return render_to_response("elections.html", {"elections":elections}, RequestContext(request))
 
 @cache_page(60 * 15)
 def election_detail(request,election_id):
 	election = Election.objects.get(id=election_id)
-	return render_to_response("basic.html",{"election":election},RequestContext(request))
+	return render_to_response("basic.html", {"election":election}, RequestContext(request))
