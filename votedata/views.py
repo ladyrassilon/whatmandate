9685f4a68f7440205da41143cd82a2a26226ceb3
Gemma Hentsch <gemma@halfapenguin.com>
2012-11-25 13:19:57 +0000

    re-enable page cache

--- votedata/views.py
+++ votedata/views.py
@@ -5,7 +5,7 @@ from votedata.models import *
 from django.template import RequestContext
 from django.views.decorators.cache import cache_page
 
-# @cache_page(60 * 15)
+@cache_page(60 * 15)
 def basic_view(request):
 	election = Election.objects.get(title="General Election 2010")
 	return render_to_response("basic.html",{"election":election},RequestContext(request))
