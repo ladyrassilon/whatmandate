9208b38eb3d024213c0f4b3a237ccab74d6e3602
Gemma Hentsch <gemma@halfapenguin.com>
2012-11-25 05:14:52 +0000

    unicode the objects

--- votedata/models.py
+++ votedata/models.py
@@ -12,29 +12,44 @@ class Election(models.Model):
 	date = models.DateField(blank=True,null=True)
 	kind = models.CharField(max_length=1,choices=ELECTION_TYPES,default="G")
 
+	def __unicode__(self):
+		return self.title
+
 class Region(models.Model):
 	name = models.CharField(max_length=100,unique=True)
+	def __unicode__(self):
+		return self.name
 
 class Constituency(models.Model):
 	name = models.CharField(max_length=100,unique=True)
 	region = models.ForeignKey(Region)
+	def __unicode__(self):
+		return "%s - %s"%(self.region.name,self.name)
 
 class PoliticalParty(models.Model):
 	name = models.CharField(max_length=100,unique=True)
+	def __unicode__(self):
+		return self.name
 
 class Candidate(models.Model):
 	name = models.CharField(max_length=200)
 	party = models.ForeignKey(PoliticalParty)
 	#slight hack as far as uniqueness for data over time/space is concerned
 	constituency_id = models.CharField(max_length=100)
+	def __unicode__(self):
+		return "%s (%s)"%(self.name,self.party.name)
 
 class ConstituencyElection(models.Model):
 	election = models.ForeignKey(Election)
 	constituency = models.ForeignKey(Constituency)
 	electorate_size = models.IntegerField()
+	def __unicode__(self):
+		return "%s (%s)"%(self.constituency.name,self.election.title)
 
 class CandidateResult(models.Model):
 	candidate = models.ForeignKey(Candidate)
 	constituency_election = models.ForeignKey(ConstituencyElection)
 	votes = models.IntegerField()
+	def __unicode__(self):
+		return "%s (%s,%s)"%(self.candidate.name,self.candidate.party.name,self.constituency_election.election)
 
