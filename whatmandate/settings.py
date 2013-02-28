0e4ca5023197b85ee378e2638af009e02e1c32b9
Gemma Hentsch <gemma@halfapenguin.com>
2013-02-28 18:23:38 +0000

    local database files

--- whatmandate/settings.py
+++ whatmandate/settings.py
@@ -152,27 +152,14 @@ LOGGING = {
 }
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default'] =  dj_database_url.config()
 
-# def get_cache():
-#   import os
-#   try:
-#     os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS']
-#     os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
-#     os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
-#     return {
-#       'default': {
-#         'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-#         'LOCATION': os.environ['MEMCACHIER_SERVERS'],
-#         'TIMEOUT': 500,
-#         'BINARY': True,
-#       }
-#     }
-#   except:
-#     return {
-#       'default': {
-#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-#       }
-#     }
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 
-CACHES = get_cache()
+from local_settings import *
+from local_database import *
